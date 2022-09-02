e02e3085d74d215ef65762326305eeae8807a7f9
chi157 <chi157@localhost>
2022-09-02 20:00:47 +0800

    sts

--- src/test/java/com/example/demo/one2many/QueryTest1.java
+++ src/test/java/com/example/demo/one2many/QueryTest1.java
@@ -1,5 +1,7 @@
 package com.example.demo.one2many;
 
+import javax.persistence.FetchType;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,6 +24,8 @@ public class QueryTest1 {
 		MenuItem m1 = menuItemRepository.findById(1L).get();
 		System.out.println(m1.getName());
 		
+		// 預設 fetch = FetchType.LAZY 只會查當下那張表(預設不加載)
+		// 注意：fetch = FetchType.LAZY 若執行時期需要加載另一張表，必須在 application.yml 裡面加入 enable_lazy_load_no_trans: true
 		System.out.println(m1.getMenuGroup().getName());
 	}
 }

c4fd1e301e6350f846472d54850f9e35f491539f
chi157 <chi157@localhost>
2022-09-02 19:26:30 +0800

    Create Repository

---
+++ src/main/java/com/example/demo/repository/MenuItemRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.one2many.twoway.MenuGroup;
+import com.example.demo.entity.one2many.twoway.MenuItem;
+
+@Repository
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long>{
+
+}
